fe334c23cc7d929eaf58544ac7b62baa539f311f
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-03-11 22:13:09 +0600

    implement fetch all issues function in app.js

--- script/app.js
+++ script/app.js
@@ -1,13 +1,19 @@
 const tabs = document.querySelectorAll('.tab-btn');
 
+const fetchAllIssues = async () => {
+    const response = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+    const data = await response.json();
+
+    return data;
+};
+
 tabs.forEach((tab) => {
     tab.addEventListener('click', (e) => {
         const currentTab = e.target;
-        
+        const tabname = currentTab.dataset.tab;
+
         tabs.forEach((btn) => {
             btn.classList.remove('active-tab');
         });
-
-        currentTab.classList.add('active-tab');
     });
 });
